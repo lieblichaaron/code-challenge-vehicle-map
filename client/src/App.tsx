3f236dc66aaba4b0bccf558653cd9ac512a09b47
Aaron Lieblich <lieblichaaron@gmail.com>
2021-11-10 17:34:16 +0200

    add location feature and class filter

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,7 @@ function App() {
   const [displayList, setDisplayList] = useState<Vehicle[]>([])
   const [chosenVehicle, setChosenVehicle] = useState<Vehicle>()
   const [filterText, setFilterText] = useState<string>('')
+  const [carClass, setCarClass] = useState<string>('')
 
   const filterVehicles = (status: 'all' | 'online' | 'in-ride') => {
     let tempList: Vehicle[] = [];
@@ -39,10 +40,10 @@ function App() {
 
   useEffect(() => {
     const tempList: Vehicle[] = vehiclesList.filter((vehicle: Vehicle) => {
-      return vehicle.vehicle_id.includes(filterText)
+      return vehicle.vehicle_id.includes(filterText) && (vehicle.class_name === carClass || carClass === 'All')
     });
     setDisplayList(tempList)
-  }, [filterText, vehiclesList])
+  }, [filterText, vehiclesList, carClass])
 
   return (
     <div className='app-container'>
@@ -55,13 +56,22 @@ function App() {
             <button onClick={() => filterVehicles('all')}>All</button>
             <button onClick={() => filterVehicles('online')}>Online</button>
             <button onClick={() => filterVehicles('in-ride')}>In ride</button>
+            <select name="class" id="class" style={{ marginRight: 5 }} onChange={(e) => setCarClass(e.target.value)}>
+              <option value="All">Class</option>
+              <option value="A">A</option>
+              <option value="B">B</option>
+              <option value="C">C</option>
+              <option value="D">D</option>
+              <option value="E">E</option>
+            </select>
             <input type="text" placeholder="Search for vehicle ID" value={filterText} onChange={(event) => setFilterText(event.target.value)} />
-          </ul>
-          {chosenVehicle && <VehiclesList vehicles={displayList} setChosenVehicle={setChosenVehicle} chosenVehicle={chosenVehicle} />}
-        </div>
+          </ul >
+          {chosenVehicle && <VehiclesList vehicles={displayList} setChosenVehicle={setChosenVehicle} chosenVehicle={chosenVehicle} />
+          }
+        </div >
         {chosenVehicle && <VehicleCard vehicle={chosenVehicle} />}
-      </div>
-    </div>
+      </div >
+    </div >
   );
 }
 
--- client/src/views/VehicleCard.tsx
+++ client/src/views/VehicleCard.tsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Vehicle } from "../utils/vehicle";
 import './VehicleCard.css'
+import Geocode from "react-geocode";
+Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY ? process.env.REACT_APP_GOOGLE_API_KEY : '')
+const VehicleCard = ({ vehicle }: { vehicle: Vehicle }) => {
+    const [location, setLocation] = useState()
 
-const VehicleCard = ({vehicle}: {vehicle: Vehicle}) => {
+    useEffect(() => {
+        Geocode.fromLatLng(`${vehicle.lat}`, `${vehicle.lng}`).then(
+            (response) => {
+                setLocation(response.results[0].formatted_address)
+            },
+            (error) => {
+                console.error(error);
+            }
+        );
+    }, [vehicle])
+
     return (
         <div className='card-container'>
-        <div className='card'>
-            <div>Vehicle Id: {vehicle.vehicle_id}</div>
-            <div>State: {vehicle.state}</div>
-            <div>Seats: {vehicle.seats}</div>
-            <div>Class: {vehicle.class_name}</div>
-            </div>
+            {location && <div className='card'>
+                <div>Vehicle Id: {vehicle.vehicle_id}</div>
+                <div>State: {vehicle.state}</div>
+                <div>Seats: {vehicle.seats}</div>
+                <div>Class: {vehicle.class_name}</div>
+                <div>Location: {location}</div>
+            </div>}
         </div>
     )
 }
